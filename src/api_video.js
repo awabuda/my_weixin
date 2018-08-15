71d496499a01fce97cf414e7ddcd46f5d8d065bf
程朋涛 <pengtao.cheng1@corp.elong.com>
2018-08-15 17:53:29 +0800

    feat(all): 增加trycatch

--- src/api_video.js
+++ src/api_video.js
@@ -14,37 +14,42 @@ module.exports = function (req,res) {
     console.log('stat-------',stat);
     console.log('fileSize---------', fileSize)
     console.log('range-------', range);
-    if (range) {
-        let parts = range.replace(/bytes=/, "").split("-");
-        console.log('parts----', parts)
-        let start = parseInt(parts[0], 10);
-        console.log('start----', start);
-        let end = parts[1] ? parseInt(parts[1], 10) : start + 999999;
-        console.log('end----', end)
-        // end 在最后取值为 fileSize - 1 
-        end = end > fileSize - 1 ? fileSize - 1 : end;
+    try{
+        if (range) {
+            let parts = range.replace(/bytes=/, "").split("-");
+            console.log('parts----', parts)
+            let start = parseInt(parts[0], 10);
+            console.log('start----', start);
+            let end = parts[1] ? parseInt(parts[1], 10) : start + 999999;
+            console.log('end----', end)
+            // end 在最后取值为 fileSize - 1 
+            end = end > fileSize - 1 ? fileSize - 1 : end;
 
-        let chunksize = (end - start) + 1;
-        let file = fs.createReadStream(vpath, {
-            start,
-            end
-        });
-        let head = {
-            'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-            'Accept-Ranges': 'bytes',
-            'Content-Length': chunksize,
-            'Content-Type': 'video/mp4',
-        };
-        console.log('dfadfasdfasdfa')
-        res.writeHead(206, head);
-        file.pipe(res);
-        console.log('读写流');
-    } else {
-        let head = {
-            'Content-Length': fileSize,
-            'Content-Type': 'video/mp4',
-        };
-        res.writeHead(200, head);
-        fs.createReadStream(vpath).pipe(res);
+            let chunksize = (end - start) + 1;
+            let file = fs.createReadStream(vpath, {
+                start,
+                end
+            });
+            let head = {
+                'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+                'Accept-Ranges': 'bytes',
+                'Content-Length': chunksize,
+                'Content-Type': 'video/mp4',
+            };
+            console.log('dfadfasdfasdfa')
+            res.writeHead(206, head);
+            file.pipe(res);
+            console.log('读写流');
+        } else {
+            let head = {
+                'Content-Length': fileSize,
+                'Content-Type': 'video/mp4',
+            };
+            res.writeHead(200, head);
+            fs.createReadStream(vpath).pipe(res);
+        }
+    }catch(e){
+        console.log(e);
     }
+    
 }
